b15effac84fc828f55450c1c604136e08433644d
vale <valenpascutti2612@gmail.com>
2025-04-25 18:03:14 -0300

    Ejercicio 2

--- 04 Estructuras Repetitivas/PASCUTTI_TP-04.py
+++ 04 Estructuras Repetitivas/PASCUTTI_TP-04.py
@@ -1,4 +1,16 @@
 # EJERCICIO 1
-
+'''
 for i in range(1, 101):
     print(i)
+'''
+
+# EJERCICIO 2
+
+numero = int(input("Ingrese un numero entero: "))
+contador = 0
+numero = str(numero)
+
+for i in range(len(numero)):
+    contador += 1
+
+print(f"Tiene {contador} digitos")

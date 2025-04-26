3bc153a9b58ec1f080d2eb8351fb0333c59606ed
vale <valenpascutti2612@gmail.com>
2025-04-26 00:16:46 -0300

    Ejercicio 3

--- 04 Estructuras Repetitivas/PASCUTTI_TP-04.py
+++ 04 Estructuras Repetitivas/PASCUTTI_TP-04.py
@@ -2,7 +2,7 @@
 '''
 for i in range(1, 101):
     print(i)
-'''
+
 
 # EJERCICIO 2
 
@@ -14,3 +14,17 @@ for i in range(len(numero)):
     contador += 1
 
 print(f"Tiene {contador} digitos")
+'''
+
+# EJERCICIO 3
+
+nro1 = int(input("Ingrese un numero: "))
+nro2 = int(input("Ingrese un numero mayor al anterior: "))
+acumulador = 0
+if nro1 < nro2:
+    for i in range(nro1 + 1, nro2):
+        acumulador += i
+
+    print(f"La suma de los nros entre el rango es: {acumulador}")
+else:
+    print("Error! El primer numero debe ser mayor que el segundo")

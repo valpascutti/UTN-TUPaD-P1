1210ec641de13afbba6674bf560421c6e6346f9e
vale <valenpascutti2612@gmail.com>
2025-04-29 20:06:42 -0300

    Ejercicio 1 y 2

---
+++ 05 Funciones/PASCUTTI_TP-05.py
@@ -0,0 +1,17 @@
+# EJERICIO 1
+'''
+lista_mult4 = []
+
+for i in range(1, 101):
+    if i % 4 == 0:
+        lista_mult4.append(i)
+    
+print(lista_mult4)
+'''
+
+# EJERCICIO 2
+
+comida = ["Hamburguese", "Lomito", "Pizza", "Asado", "Tarta"]
+
+ult_ubicacion = comida[-2]
+print(ult_ubicacion)

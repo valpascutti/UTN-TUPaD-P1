61a6dc49a9326f91e224c4eacec07e45a48a6700
vale <valenpascutti2612@gmail.com>
2025-06-14 02:08:54 -0300

    Ej 1 y 2

---
+++ 06 Datos complejos/PASCUTTI_TP-06.py
@@ -0,0 +1,15 @@
+# EJERCICIO 1
+precio_frutas = {'Banana': 1200, 'Anana': 2500, 'Melon': 3000, 'Uva': 1450}
+
+precio_frutas['Naranja'] = 1200
+precio_frutas['Manzana'] = 1500
+precio_frutas['Pera'] = 2300
+
+print(precio_frutas)
+
+# EJERCICIO 2
+precio_frutas['Banana'] = 1330
+precio_frutas['Manzana'] = 1700
+precio_frutas['Melon'] = 2800
+
+print(precio_frutas)
